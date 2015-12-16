2228ebd610b350ffab636fcc9f98057d740f097c
ajneuhar <avraham.neuhar@post.idc.ac.il>
2015-12-16 14:32:17 +0200

    Backgrounds

--- Assets/script/Level1Manager.cs
+++ Assets/script/Level1Manager.cs
@@ -5,20 +5,26 @@ public class Level1Manager : MonoBehaviour {
 
 	public GameObject enemy1;
 	public GameObject powerUp;
+	public GameObject levelButton;
+	private bool levelOver; 
 
 
 	// Use this for initialization
 	void Start () {
+		levelOver = false;
 		GameManager.numOfEnemys = 1;
 		Instantiate(enemy1, new Vector3(0f, 50f, 0f), Quaternion.identity);
 		StartCoroutine(CreatePowerUp());
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (GameManager.numOfEnemys == 0) {
+		if (GameManager.numOfEnemys == 0 && !levelOver) {
+			levelOver = true; 
 			//TODO: move to next level.
 			Debug.Log("Next level Bitch!!!!!!!!!");
+			levelButton.active = true; 
 		}
 	
 	}
@@ -27,5 +33,9 @@ public class Level1Manager : MonoBehaviour {
 	IEnumerator CreatePowerUp() {
 		yield return new WaitForSeconds(2f);
 		Instantiate(powerUp, new Vector3(13f, 50f, 0f), Quaternion.identity);
+	}
+
+	public void onClickNextLevel () {
+		Application.LoadLevel("Level2");
 	}
 }
