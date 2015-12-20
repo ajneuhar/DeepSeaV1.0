b4d351e030308d17d5c372fb95ee2d168f4f34ee
shir maimon <shirmaimonx@gmail.com>
2015-12-20 14:52:53 +0200

    Background opening screen

--- Assets/script/CanvasManager.cs
+++ Assets/script/CanvasManager.cs
@@ -2,17 +2,19 @@
 using System.Collections;
 using UnityEngine.UI;
 
+
 public class CanvasManager : MonoBehaviour {
 	
 	public GameObject canvasOne;
 	public GameObject canvasTwo;
-	
+
+
+
 	// Use this for initialization
 	void Start () {
 		canvasOne.active = true;
 	}
 
-	
 	public void onClickStartButton() {
 		Application.LoadLevel("MainScene");
 	}
